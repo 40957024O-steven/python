9da2aefc3b47716a23e9225c9fd5b5170fe2146d
40957024O-steven <stevenwsp901007@gmail.com>
2023-03-04 21:43:06 +0800

    Create 模組.py

---
+++ 基本操作/模組.py
@@ -0,0 +1,9 @@
+import moduleuse as mu
+import token #python內建模組
+import sys 
+print(mu.one)
+print(mu.tool(1,"-",9))
+print(sys.path) #尋找內建模組檔案位置 C:\Users\steve\AppData\Local\Programs\Python\Python310\Lib
+
+# import A <==引入A檔案
+# from A import B <==引入A檔案中的B模組
