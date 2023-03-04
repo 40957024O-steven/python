4bd23a05f00b849a8df9c04503ba3b63013f57a9
40957024O-steven <stevenwsp901007@gmail.com>
2023-03-05 01:52:00 +0800

    Update 統計統計.py

--- 作品/松山剖風儀/與探空統計/統計統計.py
+++ 作品/松山剖風儀/與探空統計/統計統計.py
@@ -6,12 +6,10 @@ from openpyxl import load_workbook,Workbook
 
 wbnew = Workbook()
 wsvl = wbnew.active
-wsvl.title = '風速lidar自算'
+wsvl.title = '風速lidar'
 wsvt = wbnew.create_sheet('風速探空')
-wsvm = wbnew.create_sheet('風速lidar電腦算')
-wsdl = wbnew.create_sheet('風向lidar自算')
+wsdl = wbnew.create_sheet('風向lidar')
 wsdt = wbnew.create_sheet('風向探空')
-wsdm = wbnew.create_sheet('風向lidar電腦算')
 lc = 2
 
 files = glob.glob("C:/Users/steve/Desktop/python相關資料/觀測rowdata/統計/*")
@@ -29,30 +27,28 @@ for file in files:
     # print(himax)
     for i in range(2,hivmax+1):
         wsvl.cell(i,1).value = wsv.cell(i,1).value
-        wsvl.cell(i,lc).value = wsv.cell(i,2).value
+        wsvl.cell(i,lc).value = wsv.cell(i,4).value
 
         wsvt.cell(i,1).value = wsv.cell(i,1).value
         wsvt.cell(i,lc).value = wsv.cell(i,3).value
 
-        wsvm.cell(i,1).value = wsv.cell(i,1).value
-        wsvm.cell(i,lc).value = wsv.cell(i,6).value        
+
     for i in range(2,hidmax+1):
         wsdl.cell(i,1).value = wsd.cell(i,1).value
-        wsdl.cell(i,lc).value = wsd.cell(i,2).value
+        wsdl.cell(i,lc).value = wsd.cell(i,4).value
 
         wsdt.cell(i,1).value = wsd.cell(i,1).value
         wsdt.cell(i,lc).value = wsd.cell(i,3).value
     
-        wsdm.cell(i,1).value = wsd.cell(i,1).value
-        wsdm.cell(i,lc).value = wsd.cell(i,6).value 
+
 
     wsvl.cell(1,lc).value = day
     wsvt.cell(1,lc).value = day
-    wsvm.cell(1,lc).value = day    
+    
 
     wsdl.cell(1,lc).value = day
     wsdt.cell(1,lc).value = day
-    wsdm.cell(1,lc).value = day
+
 
     lc +=1
 wbnew.save("C:/Users/steve/Desktop/python相關資料/觀測rowdata/統計.xlsx")
