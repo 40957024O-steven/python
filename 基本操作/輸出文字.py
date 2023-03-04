2dec997fa0db024f1ccd7fc6c67de30fe6be9012
40957024O-steven <stevenwsp901007@gmail.com>
2023-03-04 21:46:36 +0800

    Create 輸出文字.py

---
+++ 基本操作/輸出文字.py
@@ -0,0 +1,9 @@
+print ( "輸出文字" )
+print ( '輸出文字' )#單雙引號沒差
+word = '輸出文字'
+print ( word )
+print ( word + word )#"+"代表前後文字連接在一起
+print ( word + "輸出文字" )
+print ( word , word )#","代表中間有空格
+print ( "輸出\n文字" )#"\n"代表換行 p.s不能用在變數中：print(word \n word)<=錯誤用法
+print ( "輸出\"文字" )#'\"'代表"是文字符號而不是字串結尾
