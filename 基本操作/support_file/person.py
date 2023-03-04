253459826736971e1da5f0c3ccee7067ce3d6cd9
40957024O-steven <stevenwsp901007@gmail.com>
2023-03-04 21:42:22 +0800

    Create person.py

---
+++ 基本操作/support_file/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__ (self,name,age):
+        self.name = name
+        self.age = age
+
+    
+    def printname(self):
+        print (self.name)
+    
+    def printage(self):
+        print (self.age)
+    
+    
